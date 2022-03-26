d7198a8730519d055b7e753403f3e83cd3669031
Diego Alberto López Lazareno <if722100@iteso.mx>
2022-03-26 15:17:17 -0600

    Configuraciones iniciales
    Archivos para trabajar el proyecto.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: Proyecto de Investigación Citibanamex: BlackRock.                                          -- #
+# -- script: main.py : python script with the main functionality                                         -- #
+# -- author:                                                                                             -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/diegolazareno/ConcursoCity_BlackRock                                 -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+#%% .py 
+import functions
+
